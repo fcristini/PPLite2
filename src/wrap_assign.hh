da477e75a22add4150930d32b59e3f77e7d54588
Fabio Cristini <fcristini@gmail.com>
2017-05-01 11:11:39 +0200

    fix

--- src/wrap_assign.hh
+++ src/wrap_assign.hh
@@ -28,6 +28,7 @@ site: http://bugseng.com/products/ppl/ . */
 #include "Coefficient_defs.hh"
 #include "Variable_defs.hh"
 #include "Constraint_System_defs.hh"
+#include "Checked_Compat.hh"
 #include "assertions.hh"
 
 namespace Parma_Polyhedra_Library {
