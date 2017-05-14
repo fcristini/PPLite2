752c426a110020c80eb0d80fdd44fd72d0019521
Fabio Cristini <fcristini@gmail.com>
2017-05-14 12:13:05 +0200

    fix

--- src/checked_numeric_limits.hh
+++ src/checked_numeric_limits.hh
@@ -24,7 +24,7 @@ site: http://bugseng.com/products/ppl/ . */
 #ifndef PPL_checked_numeric_limits_hh
 #define PPL_checked_numeric_limits_hh 1
 
-#include "Checked_Number_defs.hh"
+//////#include "Checked_Number_defs.hh"
 #include "checked_int_inlines.hh"
 #include <limits>
 
@@ -32,135 +32,135 @@ namespace std {
 
 using namespace Parma_Polyhedra_Library;
 
-#define PPL_SPECIALIZE_LIMITS_INT(T)                                    \
-  /*! \brief Partial specialization of std::numeric_limits. */          \
-  template <typename Policy>                                            \
-  class numeric_limits<Checked_Number<PPL_U(T), Policy> >              \
-    : public numeric_limits<PPL_U(T)> {                                 \
-  private:                                                              \
-    typedef Checked_Number<PPL_U(T), Policy> Type;                      \
-                                                                        \
-  public:                                                               \
-    static const bool has_infinity = Policy::has_infinity;              \
-    static const bool has_quiet_NaN =  Policy::has_nan;                 \
-                                                                        \
-    static Type min() {                                                 \
-      Type v;                                                           \
-      v.raw_value() = Checked::Extended_Int<Policy, PPL_U(T)>::min;     \
-      return v;                                                         \
-    }                                                                   \
-                                                                        \
-    static Type max() {                                                 \
-      Type v;                                                           \
-      v.raw_value() = Checked::Extended_Int<Policy, PPL_U(T)>::max;     \
-      return v;                                                         \
-    }                                                                   \
-                                                                        \
-    static Type infinity() {                                            \
-      Type v;                                                           \
-      Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,  \
-                                      ROUND_IGNORE);                    \
-      return v;                                                         \
-    }                                                                   \
-                                                                        \
-    static Type quiet_NaN() {                                           \
-      Type v;                                                           \
-      Checked::assign_special<Policy>(v.raw_value(), VC_NAN,            \
-                                      ROUND_IGNORE);                    \
-      return v;                                                         \
-    }                                                                   \
-  };
+//////#define PPL_SPECIALIZE_LIMITS_INT(T)                                    \
+//////  /*! \brief Partial specialization of std::numeric_limits. */          \
+//////  template <typename Policy>                                            \
+//////  class numeric_limits<Checked_Number<PPL_U(T), Policy> >              \
+//////    : public numeric_limits<PPL_U(T)> {                                 \
+//////  private:                                                              \
+//////    typedef Checked_Number<PPL_U(T), Policy> Type;                      \
+//////                                                                        \
+//////  public:                                                               \
+//////    static const bool has_infinity = Policy::has_infinity;              \
+//////    static const bool has_quiet_NaN =  Policy::has_nan;                 \
+//////                                                                        \
+//////    static Type min() {                                                 \
+//////      Type v;                                                           \
+//////      v.raw_value() = Checked::Extended_Int<Policy, PPL_U(T)>::min;     \
+//////      return v;                                                         \
+//////    }                                                                   \
+//////                                                                        \
+//////    static Type max() {                                                 \
+//////      Type v;                                                           \
+//////      v.raw_value() = Checked::Extended_Int<Policy, PPL_U(T)>::max;     \
+//////      return v;                                                         \
+//////    }                                                                   \
+//////                                                                        \
+//////    static Type infinity() {                                            \
+//////      Type v;                                                           \
+//////      Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,  \
+//////                                      ROUND_IGNORE);                    \
+//////      return v;                                                         \
+//////    }                                                                   \
+//////                                                                        \
+//////    static Type quiet_NaN() {                                           \
+//////      Type v;                                                           \
+//////      Checked::assign_special<Policy>(v.raw_value(), VC_NAN,            \
+//////                                      ROUND_IGNORE);                    \
+//////      return v;                                                         \
+//////    }                                                                   \
+//////  };
 
-PPL_SPECIALIZE_LIMITS_INT(char)
+//////PPL_SPECIALIZE_LIMITS_INT(char)
+//////
+//////PPL_SPECIALIZE_LIMITS_INT(signed char)
+//////PPL_SPECIALIZE_LIMITS_INT(signed short)
+//////PPL_SPECIALIZE_LIMITS_INT(signed int)
+//////PPL_SPECIALIZE_LIMITS_INT(signed long)
+//////PPL_SPECIALIZE_LIMITS_INT(signed long long)
+//////
+//////PPL_SPECIALIZE_LIMITS_INT(unsigned char)
+//////PPL_SPECIALIZE_LIMITS_INT(unsigned short)
+//////PPL_SPECIALIZE_LIMITS_INT(unsigned int)
+//////PPL_SPECIALIZE_LIMITS_INT(unsigned long)
+//////PPL_SPECIALIZE_LIMITS_INT(unsigned long long)
 
-PPL_SPECIALIZE_LIMITS_INT(signed char)
-PPL_SPECIALIZE_LIMITS_INT(signed short)
-PPL_SPECIALIZE_LIMITS_INT(signed int)
-PPL_SPECIALIZE_LIMITS_INT(signed long)
-PPL_SPECIALIZE_LIMITS_INT(signed long long)
+//////#undef PPL_SPECIALIZE_LIMITS_INT
 
-PPL_SPECIALIZE_LIMITS_INT(unsigned char)
-PPL_SPECIALIZE_LIMITS_INT(unsigned short)
-PPL_SPECIALIZE_LIMITS_INT(unsigned int)
-PPL_SPECIALIZE_LIMITS_INT(unsigned long)
-PPL_SPECIALIZE_LIMITS_INT(unsigned long long)
+//////#define PPL_SPECIALIZE_LIMITS_FLOAT(T)                                  \
+//////  /*! \brief Partial specialization of std::numeric_limits. */          \
+//////  template <typename Policy>                                            \
+//////  struct numeric_limits<Checked_Number<PPL_U(T), Policy> >              \
+//////    : public numeric_limits<PPL_U(T)> {                                 \
+//////};
+//////
+//////#if PPL_SUPPORTED_FLOAT
+//////PPL_SPECIALIZE_LIMITS_FLOAT(float)
+//////#endif
+//////#if PPL_SUPPORTED_DOUBLE
+//////PPL_SPECIALIZE_LIMITS_FLOAT(double)
+//////#endif
+//////#if PPL_SUPPORTED_LONG_DOUBLE
+//////PPL_SPECIALIZE_LIMITS_FLOAT(long double)
+//////#endif
+//////
+//////#undef PPL_SPECIALIZE_LIMITS_FLOAT
 
-#undef PPL_SPECIALIZE_LIMITS_INT
+//////#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
+////////! Partial specialization of std::numeric_limits.
+//////#endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
+//////template <typename Policy>
+//////class
+//////numeric_limits<Checked_Number<mpz_class, Policy> >
+//////  : public numeric_limits<mpz_class> {
+//////private:
+//////  typedef Checked_Number<mpz_class, Policy> Type;
+//////
+//////public:
+//////  static const bool has_infinity = Policy::has_infinity;
+//////  static const bool has_quiet_NaN =  Policy::has_nan;
+//////
+//////  static Type infinity() {
+//////    Type v;
+//////    Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,
+//////                                    ROUND_IGNORE);
+//////    return v;
+//////  }
+//////
+//////  static Type quiet_NaN() {
+//////    Type v;
+//////    Checked::assign_special<Policy>(v.raw_value(), VC_NAN, ROUND_IGNORE);
+//////    return v;
+//////  }
+//////};
 
-#define PPL_SPECIALIZE_LIMITS_FLOAT(T)                                  \
-  /*! \brief Partial specialization of std::numeric_limits. */          \
-  template <typename Policy>                                            \
-  struct numeric_limits<Checked_Number<PPL_U(T), Policy> >              \
-    : public numeric_limits<PPL_U(T)> {                                 \
-};
-
-#if PPL_SUPPORTED_FLOAT
-PPL_SPECIALIZE_LIMITS_FLOAT(float)
-#endif
-#if PPL_SUPPORTED_DOUBLE
-PPL_SPECIALIZE_LIMITS_FLOAT(double)
-#endif
-#if PPL_SUPPORTED_LONG_DOUBLE
-PPL_SPECIALIZE_LIMITS_FLOAT(long double)
-#endif
-
-#undef PPL_SPECIALIZE_LIMITS_FLOAT
-
-#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
-//! Partial specialization of std::numeric_limits.
-#endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
-template <typename Policy>
-class
-numeric_limits<Checked_Number<mpz_class, Policy> >
-  : public numeric_limits<mpz_class> {
-private:
-  typedef Checked_Number<mpz_class, Policy> Type;
-
-public:
-  static const bool has_infinity = Policy::has_infinity;
-  static const bool has_quiet_NaN =  Policy::has_nan;
-
-  static Type infinity() {
-    Type v;
-    Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,
-                                    ROUND_IGNORE);
-    return v;
-  }
-
-  static Type quiet_NaN() {
-    Type v;
-    Checked::assign_special<Policy>(v.raw_value(), VC_NAN, ROUND_IGNORE);
-    return v;
-  }
-};
-
-#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
-//! Partial specialization of std::numeric_limits.
-#endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
-template <typename Policy>
-class
-numeric_limits<Checked_Number<mpq_class, Policy> >
-: public numeric_limits<mpq_class> {
-private:
-  typedef Checked_Number<mpq_class, Policy> Type;
-
-public:
-  static const bool has_infinity = Policy::has_infinity;
-  static const bool has_quiet_NaN =  Policy::has_nan;
-
-  static Type infinity() {
-    Type v;
-    Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,
-                                    ROUND_IGNORE);
-    return v;
-  }
-
-  static Type quiet_NaN() {
-    Type v;
-    Checked::assign_special<Policy>(v.raw_value(), VC_NAN, ROUND_IGNORE);
-    return v;
-  }
-};
+//////#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
+////////! Partial specialization of std::numeric_limits.
+//////#endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
+//////template <typename Policy>
+//////class
+//////numeric_limits<Checked_Number<mpq_class, Policy> >
+//////: public numeric_limits<mpq_class> {
+//////private:
+//////  typedef Checked_Number<mpq_class, Policy> Type;
+//////
+//////public:
+//////  static const bool has_infinity = Policy::has_infinity;
+//////  static const bool has_quiet_NaN =  Policy::has_nan;
+//////
+//////  static Type infinity() {
+//////    Type v;
+//////    Checked::assign_special<Policy>(v.raw_value(), VC_PLUS_INFINITY,
+//////                                    ROUND_IGNORE);
+//////    return v;
+//////  }
+//////
+//////  static Type quiet_NaN() {
+//////    Type v;
+//////    Checked::assign_special<Policy>(v.raw_value(), VC_NAN, ROUND_IGNORE);
+//////    return v;
+//////  }
+//////};
 
 } // namespace std
 
